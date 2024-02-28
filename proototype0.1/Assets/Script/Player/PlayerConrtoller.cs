4cec662a511a4b7db8c0db83adb4e4044a28df34
pyypypyyy <838108081@qq.com>
2024-02-28 03:25:10 +0000

    map confinder and camera follow

--- proototype0.1/Assets/Script/Player/PlayerConrtoller.cs
+++ proototype0.1/Assets/Script/Player/PlayerConrtoller.cs
@@ -5,45 +5,42 @@ using System.Collections.Generic;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
-   
-    private Rigidbody rb;
-    private Animator animatior;
+
+    private Rigidbody2D rb;
+    //private Animator animatior;
 
     private float inputX, inputY;
     private float stopX, stopY;
 
-    private Vector3 offset;
-   
-
     private void Start()
     {
-        offset = Camera.main.transform.position - transform.position;
-        rb = GetComponent<Rigidbody>();
-        animatior = GetComponent<Animator>();
+
+        rb = GetComponent<Rigidbody2D>();
+        //animatior = GetComponent<Animator>();
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         inputX = Input.GetAxisRaw("Horizontal");
         inputY = Input.GetAxisRaw("Vertical");
         Vector2 input = new Vector2(inputX, inputY).normalized;
         rb.velocity = input * moveSpeed;
+        /*
+                if (input != Vector2.zero)
+                {
+                    animatior.SetBool("isMoving", true);
+                    stopX = inputX;
+                    stopY = inputY;
+                }
+                else
+                {
+                    animatior.SetBool("isMoving", false);
+                }
+                animatior.SetFloat("InputX", stopX);
+                animatior.SetFloat("InputY", stopY);
 
-        if(input != Vector2.zero)
-        {
-            animatior.SetBool("isMoving", true);
-            stopX = inputX;
-            stopY = inputY;
-        }
-        else
-        {
-            animatior.SetBool("isMoving", false);
-        }
-        animatior.SetFloat("InputX", stopX);
-        animatior.SetFloat("InputY", stopY);
-
-        Camera.main.transform.position = transform.position + offset;
+        */
     }
 
-    
+
 }
