412c111a2db650bbfd832fd072a9f64ab95987bd
pyypypyyy <838108081@qq.com>
2024-03-01 10:57:53 +0000

    1

--- proototype0.1/Assets/Script/Character/Player/Player.cs
+++ proototype0.1/Assets/Script/Character/Player/Player.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : Character
+public class Player: Character
 {
     [Header("MeleeAttack")]
     public float meleeAttackDamage;
